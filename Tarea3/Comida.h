38081abd258007bb19516fca55f2d87159eff6b4
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-11-06 14:35:00 -0500

    Update Comida.h

--- Tarea3/Comida.h
+++ Tarea3/Comida.h
@@ -1,16 +1,20 @@
-//
-// Created by brayan.garcia on 31/10/2019.
-//
-
-#ifndef TAREA3_COMIDA_H
-#define TAREA3_COMIDA_H
-
-
-
+#ifndef TAREA_COMIDA_H
+#define TAREA_COMIDA_H
+#include "Estado_Animo.h"
+#include <iostream>
+using namespace std;
 class Comida {
+private:
+    string alimento;
+    int cant;
+public:
+    Comida(int cant);
+    Comida(string alimento, int animo, int cant);
+    void cantidad(int cant);
+    int animo;
+
 
 };
 
 
-
-#endif //TAREA3_COMIDA_H
+#endif //TAREA_COMIDA_H
