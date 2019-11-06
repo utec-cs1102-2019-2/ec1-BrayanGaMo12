97922fa1474009cf40000aa54e73a4a519eb7056
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-11-06 14:34:08 -0500

    Update Estado_Animo.cpp

--- Tarea3/Estado_Animo.cpp
+++ Tarea3/Estado_Animo.cpp
@@ -1,5 +1,20 @@
-//
-// Created by brayan.garcia on 31/10/2019.
-//
-
 #include "Estado_Animo.h"
+using namespace std;
+Estado_Animo::Estado_Animo(int animo) {
+    this->animo=animo;
+
+}
+void Estado_Animo::mostrar() {
+    if (animo < -5){
+        cout<<"Enojado"<<endl;
+    }
+    if (animo >= -5 && animo <= 0 ){
+        cout<<"Triste"<<endl;
+    }
+    if (animo >= 1 && animo <= 15){
+        cout<<"Feliz"<<endl;
+    }
+    if (animo > 15){
+        cout<<"JavaScript"<<endl;
+    }
+}
